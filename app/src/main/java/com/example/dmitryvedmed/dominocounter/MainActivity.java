1976997e0d27e4579036a2977688fd0a0154181b
Vedmitry7 <vedmitry7@gmail.com>
2017-07-06 20:48:05 +0300

    second commit. Finish with logic

--- app/src/main/java/com/example/dmitryvedmed/dominocounter/MainActivity.java
+++ app/src/main/java/com/example/dmitryvedmed/dominocounter/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.dmitryvedmed.dominocounter;
 
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     private ScrollView firstScroll, secondScroll;
 
+    private MediaPlayer mp;
+
+    private int[] sounds;
+
+    private boolean gameIsOver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        sounds = new int[]{R.raw.igrulia,R.raw.leha,R.raw.leha_vania,R.raw.vania};
         initView();
 
     }
@@ -32,10 +45,9 @@ public class MainActivity extends AppCompatActivity {
         secondFinalScore = (TextView) findViewById(R.id.second_final_score);
 
         firstScoreBoard = (TextView) findViewById(R.id.first_score_board);
-        firstFinalScore.setText("");
         secondScoreBoard = (TextView) findViewById(R.id.second_score_board);
 
-      //  firstScroll = (ScrollView) findViewById(R.id.first_scroll);
+        //  firstScroll = (ScrollView) findViewById(R.id.first_scroll);
  /*       firstScoreBoard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,11 +58,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View v){
 
+
+        System.out.println("C Score " + certainScore.getText());
+
+        if(gameIsOver)
+            return;
+
         if(certainScore.getText().equals("0")){
             certainScore.setText("");
         }
 
+
         switch (v.getId()){
+
             case R.id.btn_0:
                 certainScore.setText(certainScore.getText()+"0");
                 break;
@@ -85,38 +105,119 @@ public class MainActivity extends AppCompatActivity {
                 certainScore.setText("21");
                 break;
             case R.id.first_score_board:
-                if(certainScore.getText().equals(""))
+                if(certainScore.getText().equals("")){
+                    certainScore.setText("0");
                     return;
+                }
 
                 certainValue = Integer.valueOf((String) certainScore.getText());
+                if(certainValue == 21){
+                    playSound();
+                }
                 firstValue = firstValue + certainValue;
                 firstFinalScore.setText(String.valueOf(firstValue));
 
                 certainScore.setText("0");
 
-                if(firstFinalScore.getText().equals("")){
+                if(firstScoreBoard.getText().equals("")){
+                    System.out.println("EQ");
                     firstScoreBoard.setText(String.valueOf(firstValue));
                 }
-                    else {
-                    firstScoreBoard.setText(firstScoreBoard.getText() +"\r\n " + firstValue);
+                else {
+                    System.out.println("NE EQ");
+                    System.out.println(firstFinalScore);
+                    System.out.println(firstFinalScore.length());
+                    firstScoreBoard.setText(firstScoreBoard.getText() +"\r\n " + certainValue);
+                }
+
+                if(firstValue>101){
+                    certainScore.setText("ХВАТАЕТ!");
+                    gameIsOver = true;
                 }
                 break;
             case R.id.second_score_board:
-                if(certainScore.getText().equals(""))
+                if(certainScore.getText().equals("")){
+                    certainScore.setText("0");
                     return;
+                }
 
                 certainValue = Integer.valueOf((String) certainScore.getText());
+                if(certainValue == 21){
+                    playSound();
+                }
                 secondValue = secondValue + certainValue;
                 secondFinalScore.setText(String.valueOf(secondValue));
                 certainScore.setText("0");
 
-                if(secondFinalScore.getText().equals("")){
-                    secondScoreBoard.setText(secondValue);
+                if(secondScoreBoard.getText().equals("")){
+                    secondScoreBoard.setText(String.valueOf(secondValue));
                 }
                 else {
-                    secondScoreBoard.setText(secondScoreBoard.getText() +"\r\n " + secondValue);
+                    secondScoreBoard.setText(secondScoreBoard.getText() +"\r\n " + certainValue);
+                }
+
+                if(secondValue>101){
+                    certainScore.setText("ХВАТАЕТ!");
+                    gameIsOver = true;
                 }
                 break;
         }
+
+        if(!certainScore.getText().equals("")&&!certainScore.getText().equals("ХВАТАЕТ!")){
+            certainValue = Integer.valueOf((String) certainScore.getText());
+            if(certainValue > 101){
+                certainScore.setText(((String) certainScore.getText()).substring(0, certainScore.getText().length()-1));
+                return;
+            }
+        }
+        if(certainScore.getText().equals(""))
+            certainScore.setText("0");
     }
+
+    private void playSound(){
+        int i = new Random().nextInt(4);
+        System.out.println(i);
+        mp = MediaPlayer.create(MainActivity.this, sounds[i]);
+        mp.start();
+    }
+
+    public void newGame(View v){
+        if(gameIsOver){
+            firstValue = 0;
+            secondValue = 0;
+            certainScore.setText("");
+            certainValue = 0;
+            firstFinalScore.setText("0");
+            secondFinalScore.setText("0");
+            firstScoreBoard.setText("");
+            secondScoreBoard.setText("");
+            gameIsOver = false;
+        } else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Новая игра?");
+            dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    firstValue = 0;
+                    secondValue = 0;
+                    certainScore.setText("");
+                    certainValue = 0;
+                    firstFinalScore.setText("0");
+                    secondFinalScore.setText("0");
+                    firstScoreBoard.setText("");
+                    secondScoreBoard.setText("");
+                    gameIsOver = false;
+                }
+            });
+            dialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            dialog.show();
+
+        }
+    }
+
 }
